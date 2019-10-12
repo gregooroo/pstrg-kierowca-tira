5b60af39ae944a1c81e85556bbf8b6059af5e05a
Mateusz <gregooroo@gmail.com>
2019-10-12 22:42:28 +0200

    Add dotenv package - move app port to .env file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
+import 'dotenv/config'
 import app from './app'
+
+const {APP_PORT = null} = process.env
+
+if (!APP_PORT || !Number(APP_PORT)) {
+  console.error('Please specify port in .env file')
+  process.exit()
+}
 
 const server = app.listen(3000, () => {
   console.log(`Server is running on port ${server.address().port}`)

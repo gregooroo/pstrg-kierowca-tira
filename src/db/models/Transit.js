2cae54e2b02d0235d4276172517dae73e484d8d3
Mateusz <gregooroo@gmail.com>
2019-10-13 16:16:21 +0200

    Create Transit model

---
+++ src/db/models/Transit.js
@@ -0,0 +1,25 @@
+import {Schema, model} from 'mongoose'
+import {isValidDateFormat} from '../../utils/date'
+
+const TransitSchema = new Schema({
+  source_address: {
+    type: String,
+    required: [true, 'Source address is required'],
+    trim: true,
+  },
+  destination_address: {
+    type: String,
+    required: [true, 'Destination address is required'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  date: {
+    type: Date,
+    required: [true, 'Date is required'],
+  },
+})
+
+export default model('Transit', TransitSchema)

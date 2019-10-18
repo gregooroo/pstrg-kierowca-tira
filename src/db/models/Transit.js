9ac884eef4705bebf97ef434dcf0aa3447acf358
Mateusz <gregooroo@gmail.com>
2019-10-18 22:48:24 +0200

    Calculate distance before save

--- src/db/models/Transit.js
+++ src/db/models/Transit.js
@@ -1,4 +1,6 @@
 import {Schema, model} from 'mongoose'
+import mapbox from '../../mapbox'
+import {generateError} from '../../utils/errorHandlers'
 
 const TransitSchema = new Schema({
   source_address: {
@@ -19,6 +21,25 @@ const TransitSchema = new Schema({
     type: Date,
     required: [true, 'Date is required'],
   },
+  distance: {
+    type: Number,
+    default: 0,
+  },
+})
+
+TransitSchema.pre('save', function calculateDistance(next) {
+  // eslint-disable-next-line
+  const {source_address, destination_address} = this
+
+  mapbox(source_address, destination_address)
+    .then(dist => {
+      this.distance = dist
+      return next()
+    })
+    .catch(errors => {
+      const {status, name, message, ...details} = errors
+      next(generateError(status, name, message, details))
+    })
 })
 
 export default model('Transit', TransitSchema)
